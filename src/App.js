d6179700fed076eeff7af15c1404126c0775e593
skratzsch <skratzsch@yahoo.de>
2024-01-05 23:46:07 +0100

    add MainPage.jsx and basic css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import LoginPage from './components/login/LoginPage';
+//import LoginPage from './components/login/LoginPage';
+import MainPage from './components/main/MainPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <MainPage />
     </div>
   );
 }
