eaa155739e38c1c57e027b21074afd46e679650c
skratzsch <skratzsch@yahoo.de>
2024-01-05 22:42:37 +0100

    fix size of logo, append logo class

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './xPans.png';
 
 function Logo() {
-    return <div><img src={logo} alt="logo" /></div>;
+    return <div><img src={logo} alt="logo" class="logo" /></div>;
 }
 
 export default Logo;
