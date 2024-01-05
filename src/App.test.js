4b6077c08436c3640dd91f54f26f0ae68665b29b
skratzsch <skratzsch@yahoo.de>
2024-01-05 10:24:26 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders the login page', () => {
+    render(<App />);
+    expect(screen.getByLabelText(/Benutzername/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Passwort/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /einloggen/i })).toBeInTheDocument();
+  });
+
 });
---
+++ src/components/login/LoginPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './LoginPage.css'; 
+import logo from './xPans.png'; // Pfad anpassen, falls erforderlich
+
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Einloggen mit:', username, password);
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Logo" className="logo"/>
+        <div>
+          <label htmlFor="username">Benutzername:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Passwort:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">xPans!</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
